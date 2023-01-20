c496d95bb78c7edc242d0d35ac9a1e2231e3f4d9
Palloma Hortencio <palloma.hortencio@gmail.com>
2023-01-20 11:58:23 -0300

    Desafio *terminar

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+import MapView from "react-native-maps";
+import { useEffect, useState } from "react";
 
-export default function App() {
+export default function ImagePickerExample() {
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  const [foto, setFoto] = useState();
+  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
+
+  useEffect(() => {
+    async function verificarPermissao() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+      requestPermission(cameraStatus === "granted");
+    }
+    verificarPermissao();
+  }, []);
+
+  useEffect(() => {
+    async function obterLocalizacao() {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+
+      setMinhaLocalizacao(localizacaoAtual.coords);
+    }
+    obterLocalizacao();
+  }, []);
+  console.log(minhaLocalizacao);
+
+  const [localizao, setLocalizacao] = useState();
+
+  const marcarLocal = () => {};
+
+  const acessarCamera = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    });
+    console.log(imagem);
+    setFoto(imagem.assets[0].uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Acessar a câmera" onPress={acessarCamera} />
+      {foto && (
+        <Image source={{ uri: foto }} style={{ width: 300, height: 200 }} />
+      )}
     </View>
   );
 }
@@ -13,8 +57,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
